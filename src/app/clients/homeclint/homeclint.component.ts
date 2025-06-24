3c4af52335362605f972526391fa315501233f27
TheWhiteTusker <105912821+vamsi80@users.noreply.github.com>
2025-06-24 17:57:04 +0530

    latest

--- src/app/clients/homeclint/homeclint.component.ts
+++ src/app/clients/homeclint/homeclint.component.ts
@@ -1,14 +1,34 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homeclint',
   templateUrl: './homeclint.component.html',
   styleUrl: './homeclint.component.css'
 })
-export class HomeclintComponent{
-  constructor(private router: Router) {}
-  navigateTo() {
-    this.router.navigate(['/contact']);
-  }
+export class HomeclintComponent {
+
+  logos: string[] = [
+    '/assets/logo_svg/BIAL.svg',
+    '/assets/logo_svg/Boeing.svg',
+    '/assets/logo_svg/Bosch.svg',
+    '/assets/logo_svg/Cab.svg',
+    '/assets/logo_svg/Collins.svg',
+    '/assets/logo_svg/DIAGEO.svg',
+    '/assets/logo_svg/Embassy.svg',
+    '/assets/logo_svg/Gig.svg',
+    '/assets/logo_svg/Herbalife.svg',
+    '/assets/logo_svg/infosys.svg',
+    '/assets/logo_svg/Karnata tourism.svg',
+    '/assets/logo_svg/percept.svg',
+    '/assets/logo_svg/PETRONAS.svg',
+    '/assets/logo_svg/phase1.svg',
+    '/assets/logo_svg/SAUDHII.svg',
+    '/assets/logo_svg/Showtime.svg',
+    '/assets/logo_svg/TataSteel.svg',
+    '/assets/logo_svg/Tech mahindra.svg',
+    '/assets/logo_svg/The Barkery.svg',
+    '/assets/logo_svg/Thinkpro.svg',
+    '/assets/logo_svg/volvo.svg',
+    '/assets/logo_svg/Zebra Robotics.svg',
+  ];
 }
